8567e30760c4a2c72673ea378dd452875dba4992
Ujjwal Kumar <ujjawalchoudhary2016@gmail.com>
2021-01-23 23:16:20 +0530

    Create Main.java

---
+++ Linked List/Main.java
@@ -0,0 +1,18 @@
+package com.linkedlistds;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        LinkedList list = new LinkedList();         // new object of LinkedList class
+        list.insertAtBeg(1);
+        list.insertAtEnd(2);
+        list.insertAtEnd(3);
+        // for insert a node  at the end we have to go to last node
+        list.insertAtMid(2,4);    // here num is for which we have to insert an element after that
+
+     list.printList();
+
+    }
+}

3e09d1b5960ec1aef84c8685b9749023386ea608
Ujjwal Kumar <ujjawalchoudhary2016@gmail.com>
2021-01-25 03:28:53 +0530

    Update Doubly_LinkedList.java

--- Linked List/Doubly_LinkedList.java
+++ Linked List/Doubly_LinkedList.java
@@ -24,7 +24,7 @@ public class DoublyLinkedList {
         Node newNode = new Node(data);
         if(head ==  null){
             head =  newNode;
-            return;                       // return here for the returning the method
+            return;
         }
         newNode.next = head;
         head.prev = newNode;
@@ -47,6 +47,59 @@ public class DoublyLinkedList {
         newNode.prev = tempNode;
 
     }
+
+
+
+    // Insert a node in  between the node
+
+    void insertInBetween(int num,int data){
+        Node tempNode = head;
+        Node  newNode = new Node(data);
+        if(head== null){
+            head = newNode;
+            return;
+        }
+        while(tempNode.data != num)
+            tempNode = tempNode.next;
+
+
+        newNode.next = tempNode.next;
+
+        if(newNode.next != null)       // for avoiding i.e the newnode of next will not be null i.e newnode should not be last element
+        newNode.next.prev = newNode;  // prev of rightmost node i.e connected to newNOde
+        tempNode.next = newNode;
+        newNode.prev = tempNode;
+
+
+
+    }
+
+
+    // deleting the node
+
+
+    void delete(int num){
+        Node delNode = head;
+        if(head == null){
+            System.out.println("List is Empty");
+            return;
+        }
+        while(delNode!= null && delNode.data!= num)
+            delNode = delNode.next;
+        if(delNode == head){
+            head = head.next;
+            head.next.prev = null;
+            return;
+        }
+
+        delNode.prev.next = delNode.next;   
+        // only both pointing code that will delete the mid node 
+        // for deleting the  end node check the if codn. for the next of delnode not be null
+        // here this condn check the last node also
+        if(delNode.next != null)
+            delNode.next.prev = delNode.prev;
+    }
+
        // Printing the list
     void printList(){
         Node tempNode = head;

52158b1c2f5d98d189301a3f609751dad114dd03
Ujjwal Kumar <ujjawalchoudhary2016@gmail.com>
2021-02-02 01:08:07 +0530

    Update StackMain.java

--- Stack/StackMain.java
+++ Stack/StackMain.java
@@ -4,21 +4,37 @@ public class StackMain {
 
     public static void main(String[] args) {
 	// write your code here
-        StackArray stackArray = new StackArray();
-        stackArray.push(1);
-       stackArray.push(2);
+//        StackArray stackArray = new StackArray();
+//        stackArray.push(1);
+//       stackArray.push(2);
+//
+//        stackArray.push(3);
+//        int data = stackArray.pop();
+//        System.out.println("Element popped!" + " " + data);
+//        data = stackArray.pop();
+//        System.out.println("Element popped!" + " " + data);
+//        data = stackArray.pop();
+//        System.out.println("Element popped!" + " " + data);
+//        data = stackArray.pop();
+//        System.out.println("Element popped!" + " " + data);
+//        data = stackArray.pop();
+//        System.out.println("Element popped!" + " " + data);
 
-        stackArray.push(3);
-        int data = stackArray.pop();
-        System.out.println("Element popped!" + " " + data);
-        data = stackArray.pop();
-        System.out.println("Element popped!" + " " + data);
-        data = stackArray.pop();
-        System.out.println("Element popped!" + " " + data);
-        data = stackArray.pop();
-        System.out.println("Element popped!" + " " + data);
-        data = stackArray.pop();
-        System.out.println("Element popped!" + " " + data);
+     StackLinkedList stackList = new StackLinkedList();
+     stackList.push(3);
+     stackList.push(2);
+     stackList.push(1);
+
+     int data = stackList.pop();
+        System.out.println("Element popped is "  + data);
+         data = stackList.pop();
+        System.out.println("Element popped is "  + data);
+         data = stackList.pop();
+        System.out.println("Element popped is "  + data);
+         data = stackList.pop();
+        System.out.println("Element popped is "  + data);
+
+
 
     }
 
